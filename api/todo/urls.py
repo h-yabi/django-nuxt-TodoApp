9a0911bc49cf87896f836ca28f70dba87ff80615
Hiroya Yabiku <h_yabiku@atoj.co.jp>
2021-08-21 22:12:21 +0900

    urlの設定

---
+++ api/todo/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import TodoViewSet
+
+router = DefaultRouter()
+router.register('todo', TodoViewSet)
+
+urlpatterns = [path("", include(router.urls))]
