2c2ed71458b744284bba54110c28c91f9b0e9fd2
Hiroya Yabiku <h_yabiku@atoj.co.jp>
2021-08-22 21:49:13 +0900

    添付ファイルを必須じゃなしに変更

--- api/todo/models.py
+++ api/todo/models.py
@@ -9,7 +9,7 @@ class TodoModel(models.Model):
   )
   title = models.CharField(max_length=20)
   description = models.CharField(max_length=100)
-  picture = models.FileField()
+  picture = models.FileField(blank=True)
   priority = models.CharField(choices=PRIORITY, max_length=10)
 
   def __str__(self):
